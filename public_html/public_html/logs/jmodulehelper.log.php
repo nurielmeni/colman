825d9b929f312c650012db17d670f30c27b587d3
Meni Nuriel <nurielmeni@gmail.com>
2021-10-22 13:30:55 +0300

    d

--- public_html/public_html/logs/jmodulehelper.log.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2021-10-12 12:01:00 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2021-10-12T12:01:00+00:00	DEBUG 172.26.0.1	modulehelper	ModuleHelper::renderModule($module) expects a module object
